79eac5535c9dd83ac5fa126a2f20df9f6b6fbe15
Ammar <ammar.siddiqui847@gmail.com>
2020-06-28 13:30:17 -0400

    added README

---
+++ README.md
@@ -0,0 +1,7 @@
+# Forex Currency Comparison
+A Web app using Flask and Python that allows the user to compare selected currencies tracked by the Alpha Vantage API.
+
+## API and resources
+* <a href="https://gist.github.com/AmrMekkawy/a5425a4dd4e453a4eb4d">HTML Currency drop-down list</a>
+* <a href="https://rapidapi.com/alphavantage/api/alpha-vantage">Alpha Vantage API (from Rapid API)</a>
+* <a href="./physical_currency_list.csv">Currency list</a>
--- main.py
+++ main.py
@@ -8,5 +8,34 @@ app = Flask(__name__)
 
 url = "https://alpha-vantage.p.rapidapi.com/query"
 
+querystring = {
+    "function":"CURRENCY_EXCHANGE_RATE", 
+    "from_currency":"CAD",
+    "to_currency":"USD"
+    }
+
+try:
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    print(response.text)
+    json_response = response.json()['Realtime Currency Exchange Rate']
+
+    _from = f"1 {str(json_response['2. From_Currency Name'])} ({str(json_response['1. From_Currency Code'])})  equals "
+    _to = f"{str(json_response['4. To_Currency Name'])} ({str(json_response['3. To_Currency Code'])})"
+    _rate = str(json_response['5. Exchange Rate'])
+    _datetime = f"Last Updated: {str(json_response['6. Last Refreshed'])} Timezone: {str(json_response['7. Time Zone'])}"
+except:
+    _from = "Error"
+    _to, _rate, _datetime = "", "", ""
+
+'''
+for key, value in json_response.items():
+    print(f"{key[3:]}: {value}")
+'''
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+    #return render_template('index.html', _to=_to, _from=_from, _rate=_rate, _datetime=_datetime)
+
 if __name__ == "__main__":
 	app.run(debug=True)
